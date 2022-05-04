5a5b43213c2d70f56e7d61a03b5256bf6bf23e05
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-04 15:49:10 +0530

    used uselocation to get query strings

--- tutorial/react routing/routing/src/components/Profile.jsx
+++ tutorial/react routing/routing/src/components/Profile.jsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom"
+import { useParams , useLocation} from "react-router-dom"
 
-function Profile({match}) {
-  console.log(useParams())
+function Profile() {
+  const query = new URLSearchParams(useLocation().pathname)
+  const name = query.get("name")
+
   return (
     <>
       <div>
         <h1>using Use Params</h1>
         id is {useParams().id}
+      </div>
+      <div>
+        <h1>Name using useLocation and URLSearchParams</h1>
+        name is {name}
       </div>
     </>
   )
