988b2c5fb0f450230cbb75de73936163943896ff
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-04 18:57:47 +0530

    Build a protected route using Redirect

--- tutorial/react routing/routing/src/App.js
+++ tutorial/react routing/routing/src/App.js
@@ -8,17 +8,24 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import { Error } from './components/Error';
+
+import { useState } from "react";
+import { Redirect } from 'react-router-dom';
 function App() {
+  const [log, setlog] = useState(false)
+
   return (
     <BrowserRouter>
       <div class="App">
         <Header />
+        <button onClick={()=>setlog(!log)}>{log?"Logout":"login"}</button>
       </div>
       <li>
         <Link to="/">Home</Link>
       </li>
+      {/* protected route */}
       <li>
-        <Link to="/profile">profile</Link>
+        <Link to="/profile/:1245">profile</Link>
       </li>
       <li>
         <Link to="/about">about</Link>
@@ -26,7 +33,9 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
-        <Route path="/profile/:id" exact component={Profile} />
+        <Route path="/profile/:id">
+          {log?<Profile/>:<Redirect to="/"/>}
+        </Route>
 
         {/* it will be shown if the page not exists */}
         <Route component={Error}/>
