af202e96788250b50b150558c529a03ab4ff9de0
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-04 21:22:19 +0530

    added nested routing using useRouteMatch

--- tutorial/react routing/routing/src/components/About.jsx
+++ tutorial/react routing/routing/src/components/About.jsx
@@ -1,13 +1,32 @@
 import { useEffect } from "react"
 import { useHistory } from "react-router-dom"
+import { useRouteMatch } from "react-router-dom"
+import { Link , Switch , Route} from "react-router-dom"
+import Author from "./Author"
+import Authority from "./Authority"
 function About({log}) {
   const his = useHistory()
   useEffect(() => {
     if (!log) return his.push("/")
   }, [])
   
+  const {path , url} = useRouteMatch()
+  console.log(useRouteMatch())
   return (
-    <>About</>
+    <>
+      <h1>About</h1>
+      <li>
+        <Link to={`${url}/author`}>author</Link>
+      </li>
+      <li>
+        <Link to={`${url}/authority`}>authority</Link>
+      </li>
+
+      <Switch>
+        <Route path={`${path}/author`} component={Author}/>
+        <Route path={`${path}/authority`} component={Authority}/>
+      </Switch>
+    </>
   )
 }
 
---
+++ tutorial/react routing/routing/src/components/Authority.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function authority() {
+  return (
+    <div>authority</div>
+  )
+}
+
+export default authority
