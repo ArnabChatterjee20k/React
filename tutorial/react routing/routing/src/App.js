953fa9b4a2bd3cd96bc1816f0f588ef3d4648287
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-04 15:02:49 +0530

    usage browserrouter route link and exact prop and switch

--- tutorial/react routing/routing/src/App.js
+++ tutorial/react routing/routing/src/App.js
@@ -1,8 +1,33 @@
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+import Profile from './components/Profile';
 function App() {
   return (
-    "Heloow world"
+    <BrowserRouter>
+      <div class="App">
+        <Header />
+      </div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/profile">profile</Link>
+      </li>
+      <li>
+        <Link to="/about">about</Link>
+      </li>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/profile" exact component={Profile} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 export default App;
