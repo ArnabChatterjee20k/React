2603c87018a1ee97aad2a87c49354f60207e0bf6
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-19 23:07:48 +0530

    added newsletter with grid

--- tailwind/src/App.js
+++ tailwind/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Analytics } from './component/Analytics';
 import { Hero } from './component/Hero';
 import { Navbar } from './component/Navbar';
+import { NewsLetter } from './component/NewsLetter';
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
       <Analytics/>
+      <NewsLetter/>
     </>
   );
 }
