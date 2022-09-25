1f9c022a4d30336d3cd780c2ec9922695c9d764c
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-09-25 12:42:45 +0530

    Made some change

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,16 +1,13 @@
 // use state components
-import FunctionArg from "./components/state/FunctionArg"
-import Initialisation from "./components/state/Initialisation"
-import { ObjectUpdate } from "./components/state/ObjectUpdate"
-import ChangingInputs_At_Once from "./components/state/ChangingInputs_At_Once"
-import DerivedStates from "./components/state/DerivedStates"
+import StateComponent from "./components/state/StateComponent"
 
 // use effect components
 import EffectComponent from "./components/effect/EffectComponent"
 function App() {
   return (
     <div className="App">
-      <EffectComponent/>
+      {/* <StateComponent/> */}
+      {/* <EffectComponent/> */}
     </div>
   )
 }
---
+++ hooks/src/components/state/StateComponent.jsx
@@ -0,0 +1,13 @@
+import FunctionArg from "./FunctionArg"
+import Initialisation from "./Initialisation"
+import { ObjectUpdate } from "./ObjectUpdate"
+import ChangingInputs_At_Once from "./ChangingInputs_At_Once"
+import DerivedStates from "./DerivedStates"
+
+const StateComponent = () => {
+  return (
+    <DerivedStates/>
+  )
+}
+
+export default StateComponent
