301e05b2d7c8b297b42736a9727eb1b4e41774cc
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-10-07 16:02:16 +0530

    added way to create default options in query client

--- reactquery/react-query/src/query-client.jsx
+++ reactquery/react-query/src/query-client.jsx
@@ -2,4 +2,11 @@ import {
     QueryClient
   } from '@tanstack/react-query'
   
-  export const client = new QueryClient()
+  export const client = new QueryClient({
+    defaultOptions:{
+      queries:{
+        staleTime:2000,
+        refetchInterval:2000
+      }
+    }
+  })
